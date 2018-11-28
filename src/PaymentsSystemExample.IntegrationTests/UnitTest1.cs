68af1367073cdc2a906ddb6e8e2f9197db27e6d1
Michal Franc <lam.michal.franc@gmail.com>
2018-11-28 20:26:00 +0000

    Adding basic integration tests project

---
+++ src/PaymentsSystemExample.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PaymentsSystemExample.IntegrationTests
+{
+    public class BasicIntegrationTests
+    {
+        [Fact]
+        public void BasicTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
