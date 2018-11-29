d953bb2333d04608b6655c18cbfd6289a5eede48
Michal Franc <lam.michal.franc@gmail.com>
2018-11-29 22:36:42 +0000

    comments explaining what is lightbdd scope

--- src/PaymentsSystemExample.IntegrationTests/LightBddScope.cs
+++ src/PaymentsSystemExample.IntegrationTests/LightBddScope.cs
@@ -3,6 +3,7 @@ using LightBDD.XUnit2;
 
 [assembly: ConfiguredLightBddScope]
 
+// This is class is required for LightBDD runner configuration
 internal class ConfiguredLightBddScopeAttribute : LightBddScopeAttribute
 {
     protected override void OnConfigure(LightBddConfiguration configuration)
