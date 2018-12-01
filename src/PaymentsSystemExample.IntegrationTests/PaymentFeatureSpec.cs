f9c056b67120cfca98f8419d2525007b0ad4a9eb
Michal Franc <lam.michal.franc@gmail.com>
2018-12-01 15:38:14 +0000

    Experimenting with lightbdd

--- src/PaymentsSystemExample.IntegrationTests/PaymentFeatureSpec.cs
+++ src/PaymentsSystemExample.IntegrationTests/PaymentFeatureSpec.cs
@@ -6,24 +6,40 @@ using Xunit;
 namespace PaymentsSystemExample.IntegrationTests
 {
     [FeatureDescription(
-        @"As a user when I use API to get payment"
+        @"As a user when I call Payment API to fetch payment"
     )]
-    [Label("Payment Feature")]
+    [Label("Payment API Fetch")]
     public class PaymentFeatureSpec : FeatureFixture
     {
         [Scenario]
         [MultiAssert]
-        [Label("and payment doesn't exist. I get a 404 in response status code.")]
-        public void BasicTest()
+        [Label("And payment doesn't exist")]
+        public void PaymentDoesntExistTest()
         {
             Runner.RunScenario(
-                _ => Test_is_true()
+                _ => I_get_status_code(404)
             );
         }
 
-        private void Test_is_true()
+        [Scenario]
+        [MultiAssert]
+        [Label("And payment does exist")]
+        public void PaymentDoesExistTest()
         {
-            Assert.True(true);
+            Runner.RunScenario(
+                _ => I_get_status_code(200),
+                _ => I_get_payment_data_in_content()
+            );
+        }
+
+        private void I_get_status_code(int expectedStatusCode)
+        {
+            Assert.True(false);
+        }
+
+        private void I_get_payment_data_in_content()
+        {
+            Assert.True(false);
         }
     }
 }
